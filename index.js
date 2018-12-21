c18cb45209adc068aaa22e0b7e8a1786fb993988
Pourab Karchaudhuri <36023@Hexaware.com>
2018-12-21 11:15:30 +0530

    Removed Nodemailer

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 var handler = require('./handler');
-var mailer = require('./mailer');
 
 var keywords = ['Machine Learning', 'Tensorflow', 'Facial Recognition', 'Augmented Reality', 'Vuforia', 'Virtual Reality', 'Chatbots', 'Voicebots', 'Cloud Computing', 'Extended Reality', 'Unity'];
 
@@ -41,42 +40,6 @@ io.on('connection', function(socket){
 		}
 	});
 });
-
-app.post('/mail', function (req, res) {
-	// console.log("Event : " + JSON.stringify(req.body))
-	let fname = req.body.title.replace(/ /g,"_") + '.txt';
-	let fpath = './transcripts/'+fname;
-	fs.writeFile(fpath, req.body.payload, (err) => {  
-		// throws an error, you could also catch it here
-		if (err){
-			res.status(500).json({
-				error: true,
-				message: err
-			})
-		}	
-		// success case, the file was saved
-		console.log('Transcript saved!');
-		    mailer.Mailer(req.body.to, req.body.title, fname, fpath, (error, result)=>{
-			console.log(result);
-			if(error){
-				res.status(500).json({
-					error: true,
-					message: error
-				})
-			}
-			else{
-				res.status(200).json({
-					error: false,
-					message: result
-					
-				})
-			}
-		})
-	});
-
-
-
-})
 
 http.listen(port, function(){
 	console.log("Express server listening at PORT : ", port);
